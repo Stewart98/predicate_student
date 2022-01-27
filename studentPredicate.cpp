3b56de37b96ca6a48291f4002fcf9f605387cf9a
Zwivhuyazwanga <97505131+Zwivhuyazwanga@users.noreply.github.com>
2022-01-27 21:56:59 +0200

    Update studentPredicate.cpp

--- studentPredicate.cpp
+++ studentPredicate.cpp
@@ -8,15 +8,15 @@ int main()
     
     int marks,predicate_marks ,test1,test2;
    
-    
+    cout<<"Welcome to TUT’s exam portal!!"<<endl;
     cout<<"Enter your test 1 marks:";
     cin>>test1;
     cout<<"Enter your test 2 marks:";
     cin>>test2;
     predicate_marks=(test1+test2)/2;
-    if( predicate_marks>=40&&predicate_marks<=100)
+    if( predicate_marks>=40 && predicate_marks ==100)
     {
-    cout<<"you have qualify to write exam with "<<predicate_marks<<"%"<<endl;
+    cout<<"you have qualified to write exam with "<<predicate_marks<<"%"<<endl;
     cout<<"Enter the marks you obtain in the exam:";
     cin>>marks;
     }
@@ -24,6 +24,7 @@ int main()
     else 
     { 
         cout << " You do not qualify to write this exam " << endl;
+        cout<< "sorry, try again next semester"<<endl;
     }
     
 
