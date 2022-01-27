19ad88d62ccc46d60e69c182bcdaf9b795de2af4
Stewart98 <97505661+Stewart98@users.noreply.github.com>
2022-01-27 21:01:32 +0200

    Update studentPredicate.cpp
    else you do not qualify

--- studentPredicate.cpp
+++ studentPredicate.cpp
@@ -14,17 +14,14 @@ int main()
     cout<<"Enter your test 2 marks:";
     cin>>test2;
     predicate_marks=(test1+test2)/2;
-    if( predicate_marks>=40)
+    if( predicate_marks>=40&&predicate_marks<=100)
     {
     cout<<"you have qualify to write exam with "<<predicate_marks<<"%"<<endl;
     cout<<"Enter the marks you obtain in the exam:";
     cin>>marks;
     }
-    else if (predicate_marks >= 75)
-    {
-        cout << " You qualified with a distiction " << endl;
-    }
-    else if (predicate_marks < 40)
+    
+    else 
     { 
         cout << " You do not qualify to write this exam " << endl;
     }
