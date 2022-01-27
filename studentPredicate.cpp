c65de5cc405f4d29d98deac2cff535a5f50eae85
simelane lucky <74796644+216071433@users.noreply.github.com>
2022-01-27 23:07:20 +0200

    Update studentPredicate.cpp

--- studentPredicate.cpp
+++ studentPredicate.cpp
@@ -7,24 +7,30 @@ int main()
     // data type
     
     int marks,predicate_marks ,test1,test2;
+    int i = 0, finalMark;
    
     cout<<"Welcome to TUT’s exam portal!!"<<endl;
-    cout<<"Enter your test 1 marks:";
+    cout<<"Enter your test 1 marks:" << endl;
     cin>>test1;
-    cout<<"Enter your test 2 marks:";
+    cout<<"Enter your test 2 marks:" << endl;
     cin>>test2;
     predicate_marks=(test1+test2)/2;
-    if( predicate_marks>=40 && predicate_marks ==100)
-    {
-    cout<<"you have qualified to write exam with "<<predicate_marks<<"%"<<endl;
-    cout<<"Enter the marks you obtain in the exam:";
-    cin>>marks;
-    }
     
-    else 
-    { 
-        cout << " You do not qualify to write this exam " << endl;
-        cout<< "sorry, try again next semester"<<endl;
+    for( int i = 0; i <= 5; i++)
+    {
+         if( predicate_marks >= 40 )
+         {
+             cout<<"you have qualified to write exam with "<< predicate_marks << "%" <<endl;
+             cout<<"Enter the marks you obtain in the exam:" << endl;
+             cin>> marks;
+             finalMark = predicate_marks + ( marks / 2 );
+             cout << " your final mark is " << finalMark << "%" << endl;
+         } 
+         else 
+         { 
+             cout << " You do not qualify to write this exam " << endl;
+             cout<< "sorry, try again next semester"<<endl;
+         }
     }
     
 
